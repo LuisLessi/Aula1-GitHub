82ea5b7cfcbb914409edfe633390c1530a3b2fae
LuisLessi <luisctiunesp@hotmail.com>
2020-07-24 15:38:35 -0300

    acrescentado mensagem boa tarde

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ public class Program {
 	public static void main(String[] args) {
 		System.out.println("Good Day");
 		System.out.println("Good nigth");
+		System.out.println("Good tarde");
 	}
 
 }
